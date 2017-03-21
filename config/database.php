7b06f637bd64ab8980c6a31eaf225becf8970847
lekz974 <ahoareau@student.42.fr>
2017-03-21 09:39:08 +0100

    config

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 $DB_DSN = 'mysql:host=localhost:3306';
-$DB_USER = 'pma';
-$DB_PASSWORD = 'w4oT;v,F';
+$DB_USER = 'root';
+$DB_PASSWORD = 'root';
 $DB_NAME = 'camagru';
 $DB_TABLE = [
     'users' => 'users',
